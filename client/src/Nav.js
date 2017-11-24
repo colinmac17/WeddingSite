640c33d88319640fee4ea9bb266d71b799693bf7
Colin McAtee <colinmac@umich.edu>
2017-11-24 16:03:16 -0600

    makes title appear in one row on mobile

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
     return (
         <div className="container">
-            <MainTitle className="center-align"><Link to="/" className="nav-links">Colin and Kelsey</Link></MainTitle>
+            <MainTitle className="center-align colin-and-kelsey"><Link to="/" className="nav-links">Colin and Kelsey</Link></MainTitle>
             <NavLinks>
                 <NavLink color="#046487"><Link className="nav-links hoverable" to="/">Home</Link></NavLink>
                 <NavLink color="slategray"><Link className="nav-links hoverable" to="/details">Details</Link></NavLink>

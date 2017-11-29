815eb07c827702b491dbb8d31a171b6ea9da5e0d
Colin McAtee <colinmac@umich.edu>
2017-11-28 22:01:17 -0600

    fixes typos

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         <Col m={6} xs={12}>
           <SmallTitle>Our Story</SmallTitle>
           <HR borderBottom="3px solid #3e3e3e" />
-          <p>Colin and Kelsey first met during May of 2014 while staying in Ann Arbor for the Spring Term. Kelsey lived in the house across the street from me on Greenwood Ave. for the summer, which happened to be a mutual friends home. At first they were just friends, and actually always joked how they didn't really even like eachother. However, by the end of the summer, they were constantly going to visit her at work and going on coffee dates in the mornings. The rest is history! </p>
+          <p>Colin and Kelsey first met during May of 2014 while staying in Ann Arbor for the Spring Term. Kelsey lived in the house across the street from Colin on Greenwood Ave for the summer, which happened to be a mutual friends home. At first they were just friends, and actually always joked how they didn't really even like eachother. However, by the end of the summer, they were constantly going to visit her at work and going on coffee dates in the mornings. The rest is history! </p>
         </Col>
       </Row>
       <br/><br/>
@@ -27,7 +27,7 @@ class App extends Component {
         <Col m={6} xs={12}>
           <SmallTitle>The Proposal</SmallTitle>
           <HR borderBottom="3px solid #3e3e3e" />
-          <p>Colin and Kelsey had talked about getting married someday for sometime and while they were not shy about the subject, Kelsey always joked that Colin could not propose to her on our Europe trip. So, in perfect Colin fashion, he decided to propose to her in Europe on their first full day on the trip. He really wanted the rest of the trip to be a celebration and the stress to be off his back. He did my best to make it the perfect moment for her - he made sure no one was around, that it was a beautiful day in a romantic setting and that I got a picture! He actually ended up setting up my phone and recording the whole thing. This was the most perfect moment in their lives together so far.</p>
+          <p>Colin and Kelsey had talked about getting married someday for sometime and while they were not shy about the subject, Kelsey always joked that Colin could not propose to her on their planned trip to Europe in September 2017. So in perfect Colin fashion, he decided to propose to her in Europe on their first full day on the trip. He really wanted the rest of the trip to be a celebration and the stress to be off his back. He did his best to make it the perfect moment for her - he made sure no one was around, that it was a beautiful day in a romantic setting and that he got a picture! He actually ended up setting up his phone and recording the whole thing. This was the most perfect moment in their lives together so far.</p>
         </Col>
       </Row>
       <br/><br/>

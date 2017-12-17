a59e794156770deb83f4dba153254d6167961261
Colin McAtee <colinmac@umich.edu>
2017-12-17 11:42:44 -0600

    updated rsvp

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-//const http = require('http');
+require('dotenv').config();
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
